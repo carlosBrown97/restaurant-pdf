8c96e9d70523c8cb6c9d5496e50e1483af158627
Carlos Brown <cbrownc97@gmail.com>
2020-11-24 12:24:03 -0300

    add connection

---
+++ src/connection.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql')
+const config = require('../config.json')
+
+var connection = mysql.createConnection({
+  host: config.DB_HOST,
+  user: config.DB_USER,
+  password: config.DB_PASSWORD,
+  database: config.DB_DATABASE
+})
+
+connection.connect()
+
+connection.query('SELECT * FROM orders_completed', function(err, rows) {
+  if (err) {
+    console.log('ERROR', err.stack)
+  }
+  console.log('DATA', rows)
+})
+
+connection.end()
