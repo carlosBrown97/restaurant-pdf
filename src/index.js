e4467015bfab7d0db4d6cccbb478c2eca64e87e7
Carlos Brown <cbrownc97@gmail.com>
2020-12-03 15:25:28 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const routes = require('./routes/index')
 const app = express();
+const cors = require('cors')
 
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(routes)
+app.use(cors())
 
 app.listen(PORT, () => {
   console.log('Listen on port 3000')
