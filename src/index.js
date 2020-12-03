f62788e92c9d2c7f1d89aa734c27cf4b04babfc5
Carlos Brown <cbrownc97@gmail.com>
2020-12-03 15:38:55 -0300

    fix cors

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const cors = require('cors')
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
-app.use(routes)
 app.use(cors())
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log('Listen on port 3000')
